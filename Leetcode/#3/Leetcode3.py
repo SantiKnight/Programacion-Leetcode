038d7572814049947bba371b9b5041bc1e25df20
SantiKnight <santiknight2003@gmail.com>
2024-06-07 10:22:33 -0300

    organization changes

--- Leetcode/#3/Leetcode3.py
+++ Leetcode/#3/Leetcode3.py
@@ -4,7 +4,6 @@ class Solution:
         reversed_x = list(str(x))
         reversed_x = list(reversed_x)
         reversed_x.reverse()
-        print(num, reversed_x)
         if num == reversed_x:
             return True
         else:
--- Leetcode/#5/Leetcode5.py
+++ Leetcode/#5/Leetcode5.py
@@ -9,12 +9,10 @@ class Solution:
         for l in abc2:
             abc[l] = score[index]
             index += 1
-        #HASTA ACA ESTA BIEN
         letters_count: list[int] = [0 for _ in range(26)]
         for letter in letters:
             ind: int = ord(letter) - 97
             letters_count[ind] += 1
-        print(letters_count)
         
         words_scores: dict[str, int] = {}
         for word in words:
@@ -23,8 +21,6 @@ class Solution:
                 ind = ord(char) - 97
                 s += score[ind]
             words_scores[word] = s
-            
-        print(words_scores)
         
         lAux = lettersAux
         for word in words:
@@ -46,10 +42,6 @@ class Solution:
                         flag = False
                         lAux = lettersAux
                         break
-                    
-                    #la concha de tu madre
-                    
-            print(word, wordValue)
             totalValue += wordValue
             print(totalValue)
 s = Solution()
