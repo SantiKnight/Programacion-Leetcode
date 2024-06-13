7b975877f54e243991ba1112af034eee841c2d8b
SantiKnight <santiknight2003@gmail.com>
2024-06-13 14:15:09 -0300

    Updated Leetcode #28

--- Leetcode/#28/Leetcode28.py
+++ Leetcode/#28/Leetcode28.py
@@ -1,12 +1,7 @@
 class Solution:
     def minMovesToSeat(self, seats: list[int], students: list[int]) -> int:
-        total = 0
-        seats = sorted(seats)
-        students = sorted(students)
-        for student in students:
-            dif = abs(student-seats[0])
-            seats.pop(0)
-            total += dif
-        return total
+        seats.sort()
+        students.sort()
+        return sum(abs(students[i]-seats[i])for i in range(len(students)))
 s = Solution()
 s.minMovesToSeat([12,14,19,19,12],[19,2,17,20,7])
