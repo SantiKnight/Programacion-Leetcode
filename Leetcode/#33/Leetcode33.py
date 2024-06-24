1c416c4d2447c2b81cfb44eb4337d7e55224f271
SantiKnight <santiknight2003@gmail.com>
2024-06-24 10:47:49 -0300

    Completed unsolved Problem #33

--- Leetcode/#33/Leetcode33.py
+++ Leetcode/#33/Leetcode33.py
@@ -1,9 +1,20 @@
-import math
 class Solution:
     def maxDistance(self, position: list[int], m: int) -> int:
-        # suma = 0
-        # position = sorted(position)
-        # for i in range(len(position)):
-        #     print(position[i])
-# s = Solution()
-# s.maxDistance([1,7,3,4,2],3)
+        position.sort()
+        l, r = 1, position[-1] - position[0]
+        ans = -1
+        while l <= r:
+            mid = l + (r - l) // 2
+            last_position, balls = position[0], 1
+            for i in range(1, len(position)):
+                if position[i] - last_position >= mid:
+                    last_position = position[i]
+                    balls += 1
+            if balls >= m:
+                ans = mid
+                l = mid + 1
+            else:
+                r = mid - 1
+        return ans
+s = Solution()
+s.maxDistance([1,7,3,4,2],3)
